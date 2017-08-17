0a0cfeea1914ef12a1ab079ba19fedcd30a48412
wangshuang <wangshuang415265521@gmail.com>
2017-08-17 09:46:59 +0800

    Define Your Data Model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,37 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by wangshuang on 2017/8/16.
+//  Copyright © 2017年 wangshuang. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must to be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,6 +11,35 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     
+    //MARK: Meal Class Tests
+    
+    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Empty rating
+        let emptyRatingMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+    }
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
